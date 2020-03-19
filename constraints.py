48a1b3983d7b2c4dd0f7682c0d71f802bdddaffd
blawney <blawney@hsph.harvard.edu>
2020-03-18 22:27:32 -0400

    Fixing more typos

--- constraints.py
+++ constraints.py
@@ -7,17 +7,17 @@ def check_constraints(implemented_constraint, inputs_json_path):
     For this workflow, we can impose a constraint on the number of samples we will
     allow for processing.
 
-    The length of the PairedRnaSeqAndDgeWorkflow.r1_files key will be used as the
+    The length of the PairedRnaSeqAndQuantWorkflow.r1_files key will be used as the
     determinant of that number
     '''
 
     # load the inputs json:
     j = json.load(open(inputs_json_path))
     try:
-        fastq_list = j['PairedRnaSeqAndDgeWorkflow.r1_files']
+        fastq_list = j['PairedRnaSeqAndQuantWorkflow.r1_files']
     except KeyError:
         # The chances of reaching this are very unlikely, but we are being extra careful here
-        print('This should not happen-- the PairedRnaSeqAndDgeWorkflow.r1_files key should be present in your inputs JSON file')
+        print('This should not happen-- the PairedRnaSeqAndQuantWorkflow.r1_files key should be present in your inputs JSON file')
         return False
 
     # implemented_constraint is of type ImplementedConstraint and represents the base class for the actual constraint types
